d59950278b08ac1b5d32cf6f8d63623021e02be4
Samrawit02 <hairaer02@gmail.com>
2022-10-03 15:34:42 +0300

    creating services and agency model, service and service implementation

--- src/main/java/com/ethioclicks/skilledApp/businesslogic/service/AgencyService.java
+++ src/main/java/com/ethioclicks/skilledApp/businesslogic/service/AgencyService.java
@@ -1,8 +1,7 @@
 package com.ethioclicks.skilledApp.businesslogic.service;
 
-import com.ethioclicks.skilledApp.businesslogic.entity.Agency;
 import com.ethioclicks.skilledApp.businesslogic.model.AgencyModel;
-import org.springframework.stereotype.Service;
+
 
 public interface AgencyService {
     AgencyModel saveAgency(AgencyModel agencyModel,String pid );
